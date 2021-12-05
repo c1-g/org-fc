77101494015a6afae13e3e17ed34e607d097e355
c1-g <char1iegordon@protonmail.com>
2021-12-05 18:42:23 +0700

    Add intenal get entry function

--- org-fc-browser.el
+++ org-fc-browser.el
@@ -53,6 +53,11 @@ a list of vector for it.")
   (interactive)
   (org-fc-browser-draw-buffer org-fc-browser-context))
 
+(defun org-fc-browser--get-current-card ()
+  "docstring"
+  (nth (1- (line-number-at-pos)) tabulated-list-entries))
+
+
 (defun org-fc-browser-draw-buffer (context)
   "Draw a buffer with a list of all cards for CONTEXT."
   (let* ((buf (get-buffer-create org-fc-browser-buffer-name))
