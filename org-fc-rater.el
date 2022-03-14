bb3602fc777146c6ffb14e49c53587f319b856ca
c1-g <char1iegordon@protonmail.com>
2022-03-14 07:00:23 +0700

    Add org-fc-rater.el

---
+++ org-fc-rater.el
@@ -0,0 +1,52 @@
+;;; org-fc-rater.el --- Rating button in the minibuffer  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  c1-g
+
+;; Author: c1-g <char1iegordon@protonmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defvar org-fc-rater-buffer nil
+  "The current Rater buffer, or nil.")
+
+(defun org-fc-rater-setup-button ()
+  (setq org-fc-rater-buffer (get-buffer-create "*Rater*"))
+  (unwind-protect
+      (save-window-excursion
+        (require 'electric) (message nil)
+        (let ((echo-keystrokes 0)
+              (garbage-collection-messages nil))
+          (set-window-buffer (minibuffer-window) org-fc-rater-buffer)
+          (select-window (minibuffer-window))
+          (org-fc-rater-redisplay)))
+    (when org-fc-rater-buffer
+      (kill-buffer org-fc-rater-buffer)
+      (setq org-fc-rater-buffer nil))))
+
+(defun org-fc-rater-redisplay ()
+  (let ((inhibit-read-only t))
+    (with-current-buffer org-fc-rater-buffer
+      (delete-region (point-min) (point-max))
+      )))
+
+
+(provide 'org-fc-rater)
+;;; org-fc-rater.el ends here
